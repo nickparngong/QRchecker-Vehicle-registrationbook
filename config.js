ad0a9879771d06848dc63d695a10a52ebfdc7dff
nickparngong <nickparngong@gmail.com>
2026-09-10 18:00:47 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.BARCODE_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbzAcQnJS1R5yg2WQXdn3SiSjPvN_zPR4_zakVyyNOBM6hU7B1qA9dysQ2GjLb1Gb2CSXw/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbyM7emX7Ot_JAamUG1TV3c3QooWjPErx1jk2ZpcJVw86TGyJ9B0q0a2tigKJvZwnQ2rag/exec",
   apiKey: "",
   spreadsheetId: "1rfd7D_NraAkv-0zhnKUfWYtGNg0OPsDH0B7QDcsUZ3A"
 };
